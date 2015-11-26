dc99c9fced2414d0333d753e02a83e895e58ef32
keelan.clarke91 <keelan.clarke91@outlook.com>
2015-11-26 09:16:31 +0000

    Basic interface

---
+++ Ai/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Ai
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.configureToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabMain = new System.Windows.Forms.TabControl();
+            this.tabComs = new System.Windows.Forms.TabPage();
+            this.tabConcept = new System.Windows.Forms.TabPage();
+            this.tabGUI = new System.Windows.Forms.TabPage();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.tabMain.SuspendLayout();
+            this.tabComs.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(328, 22);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(212, 29);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Something Unique";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem,
+            this.settingsToolStripMenuItem,
+            this.configureToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(908, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // configureToolStripMenuItem
+            // 
+            this.configureToolStripMenuItem.Name = "configureToolStripMenuItem";
+            this.configureToolStripMenuItem.Size = new System.Drawing.Size(72, 20);
+            this.configureToolStripMenuItem.Text = "Configure";
+            // 
+            // tabMain
+            // 
+            this.tabMain.Controls.Add(this.tabComs);
+            this.tabMain.Controls.Add(this.tabConcept);
+            this.tabMain.Controls.Add(this.tabGUI);
+            this.tabMain.Location = new System.Drawing.Point(132, 75);
+            this.tabMain.Name = "tabMain";
+            this.tabMain.SelectedIndex = 0;
+            this.tabMain.Size = new System.Drawing.Size(752, 249);
+            this.tabMain.TabIndex = 2;
+            // 
+            // tabComs
+            // 
+            this.tabComs.Controls.Add(this.panel1);
+            this.tabComs.Location = new System.Drawing.Point(4, 22);
+            this.tabComs.Name = "tabComs";
+            this.tabComs.Padding = new System.Windows.Forms.Padding(3);
+            this.tabComs.Size = new System.Drawing.Size(744, 223);
+            this.tabComs.TabIndex = 0;
+            this.tabComs.Text = "Communicate";
+            this.tabComs.UseVisualStyleBackColor = true;
+            // 
+            // tabConcept
+            // 
+            this.tabConcept.Location = new System.Drawing.Point(4, 22);
+            this.tabConcept.Name = "tabConcept";
+            this.tabConcept.Padding = new System.Windows.Forms.Padding(3);
+            this.tabConcept.Size = new System.Drawing.Size(744, 185);
+            this.tabConcept.TabIndex = 1;
+            this.tabConcept.Text = "Concepts";
+            this.tabConcept.UseVisualStyleBackColor = true;
+            // 
+            // tabGUI
+            // 
+            this.tabGUI.Location = new System.Drawing.Point(4, 22);
+            this.tabGUI.Name = "tabGUI";
+            this.tabGUI.Padding = new System.Windows.Forms.Padding(3);
+            this.tabGUI.Size = new System.Drawing.Size(744, 185);
+            this.tabGUI.TabIndex = 2;
+            this.tabGUI.Text = "Graphical Display";
+            this.tabGUI.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblOutput);
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Controls.Add(this.btnEnter);
+            this.panel1.Controls.Add(this.txtInput);
+            this.panel1.Controls.Add(this.lblInput);
+            this.panel1.Location = new System.Drawing.Point(83, 40);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(441, 153);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Location = new System.Drawing.Point(43, 14);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(31, 13);
+            this.lblInput.TabIndex = 0;
+            this.lblInput.Text = "Input";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(46, 30);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 1;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(46, 117);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(100, 23);
+            this.btnEnter.TabIndex = 2;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(202, 44);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(199, 14);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(39, 13);
+            this.lblOutput.TabIndex = 4;
+            this.lblOutput.Text = "Output";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(908, 336);
+            this.Controls.Add(this.tabMain);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabMain.ResumeLayout(false);
+            this.tabComs.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem configureToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabMain;
+        private System.Windows.Forms.TabPage tabComs;
+        private System.Windows.Forms.TabPage tabConcept;
+        private System.Windows.Forms.TabPage tabGUI;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblInput;
+    }
+}
+
